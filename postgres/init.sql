95fd8e5b6ae73e5f60ad7641d9fdf084d91514ab
Dwaipayan Chowdhury <dwai1714@DC-Mac.local>
2018-03-13 16:54:16 +0530

    added docker compose

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,1 +1,15 @@
 create table sometable(id int, name text);
+CREATE ROLE tdaruwalla WITH LOGIN PASSWORD 'tdaruwalla'	;
+CREATE DATABASE emdm_d;
+ \connect emdm_d;
+ CREATE SCHEMA emdm;
+ grant usage on schema emdm to tdaruwalla;
+ GRANT ALL PRIVILEGES ON DATABASE emdm_d TO tdaruwalla;
+ CREATE TABLE emdm.t_item (product_id SERIAL NOT NULL, description CHARACTER VARYING(100) NOT NULL, 
+	urls JSONB, PRIMARY KEY (product_id));
+grant select, insert, update, delete on all tables in schema emdm to tdaruwalla;
+
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA emdm TO tdaruwalla;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA emdm TO tdaruwalla;
+
